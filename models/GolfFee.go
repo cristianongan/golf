664fd80642858c2460f0e97cf0b584e1fccbb8ef
Cao Van Hai <caohaibk92@gmail.com>
2022-03-24 17:49:24 +0700

    udp

---
+++ models/GolfFee.go
@@ -0,0 +1,9 @@
+package models
+
+// Loại thẻ
+type GolfFee struct {
+	ModelId
+	PartnerUid string `json:"partner_uid" gorm:"type:varchar(100);index"` // Hang Golf
+	CourseUid  string `json:"course_uid" gorm:"type:varchar(256);index"`  // San Golf
+	Name       string `json:"name" gorm:"type:varchar(256)"`              // Ten Loai Member Card
+}
