8fa86ec98d3f1c4a83183c087af18a2e151081ca
Cao Van Hai <caohaibk92@gmail.com>
2022-05-12 15:43:15 +0700

    udp cus

--- controllers/request/RCustomerUser.go
+++ controllers/request/RCustomerUser.go
@@ -5,6 +5,6 @@ type GetListCustomerUserForm struct {
 	PartnerUid  string `form:"partner_uid"`
 	CourseUid   string `form:"course_uid"`
 	Type        string `form:"type"`
-	CustomerUid string `json:"customer_uid"`
-	Name        string `json:"name"`
+	CustomerUid string `form:"customer_uid"`
+	Name        string `form:"name"`
 }
