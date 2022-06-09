f3e7e443ad276be1e98e6beb5576d47be367722f
Cao Van Hai <caohaibk92@gmail.com>
2022-06-09 09:27:34 +0700

    udp log

--- datasources/gormds.go
+++ datasources/gormds.go
@@ -20,10 +20,11 @@ func MySqlConnect() {
 	newLogger := logger.New(
 		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
 		logger.Config{
-			SlowThreshold:             time.Second, // Slow SQL threshold
-			LogLevel:                  logger.Info, // Log level
-			IgnoreRecordNotFoundError: true,        // Ignore ErrRecordNotFound error for logger
-			Colorful:                  false,       // Disable color
+			SlowThreshold: time.Second, // Slow SQL threshold
+			// LogLevel:                  logger.Info, // Log level: show log
+			LogLevel:                  logger.Silent, // Log level: disable log
+			IgnoreRecordNotFoundError: true,          // Ignore ErrRecordNotFound error for logger
+			Colorful:                  false,         // Disable color
 		},
 	)
 
