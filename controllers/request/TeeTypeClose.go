df845c42757a11d5ae4c7958310c752b655406ab
tuanduy183 <70496851+tuanduy183@users.noreply.github.com>
2022-08-01 17:40:33 +0700

    udp

--- controllers/request/TeeTypeClose.go
+++ controllers/request/TeeTypeClose.go
@@ -12,6 +12,6 @@ type GetListTeeTypeClose struct {
 	PageRequest
 	PartnerUid       string `form:"partner_uid"`
 	CourseUid        string `form:"course_uid"`
-	BookingSettingId *int64 `json:"booking_setting_id"`
+	BookingSettingId *int64 `form:"booking_setting_id"`
 	DateTime         string `form:"date_time"`
 }
