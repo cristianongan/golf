ddf428247e01fdd0afe224a919872007a13c6677
Cao Van Hai <caohaibk92@gmail.com>
2022-08-01 10:34:13 +0700

    udp form date

--- controllers/CBookingSetting.go
+++ controllers/CBookingSetting.go
@@ -358,7 +358,7 @@ func (_ *CBookingSetting) GetListBookingSettingOnDate(c *gin.Context, prof model
 
 	if form.OnDate != "" {
 		// Lấy ngày hiện tại
-		fromInt := utils.GetTimeStampFromLocationTime("", constants.DATE_FORMAT, form.OnDate)
+		fromInt := utils.GetTimeStampFromLocationTime("", constants.DATE_FORMAT_1, form.OnDate)
 		from = fromInt
 		to = from + 24*60*60
 	}
