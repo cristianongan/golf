5bc91efa2dc2854461592537b2d11fad367ee312
lupin9898 <nquocanh105@gmail.com>
2022-08-11 23:21:03 +0700

    update model caddie fee

--- controllers/request/RCaddieFee.go
+++ controllers/request/RCaddieFee.go
@@ -3,7 +3,7 @@ package request
 type GetListCaddieFee struct {
 	PartnerUid string `form:"partner_uid" binding:"required"`
 	CourseUid  string `form:"course_uid" binding:"required"`
-	Month      string `json:"month" binding:"required"`
+	Month      string `form:"month" binding:"required"`
 	CaddieName string `json:"Caddie_Name"`
 	CaddieCode string `json:"Caddie_Code"`
 }
