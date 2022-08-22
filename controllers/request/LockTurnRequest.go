e0dc802bc4bb806f3d981ab03af285b9ad5a6da0
tuanduy183 <70496851+tuanduy183@users.noreply.github.com>
2022-08-22 16:02:59 +0700

    update lock turn

--- controllers/request/LockTurnRequest.go
+++ controllers/request/LockTurnRequest.go
@@ -1,16 +1,18 @@
 package request
 
 type CreateLockTurn struct {
-	PartnerUid    string `json:"partner_uid" binding:"required"`
-	CourseUid     string `json:"course_uid" binding:"required"`
-	TeeTime       string `json:"tee_time" binding:"required"`
-	TeeTimeStatus string `json:"tee_time_status" binding:"required"`
-	BookingDate   string `json:"booking_date" binding:"required"`
-	Note          string `json:"note"`
+	PartnerUid     string `json:"partner_uid" binding:"required"`
+	CourseUid      string `json:"course_uid" binding:"required"`
+	TeeTime        string `json:"tee_time" binding:"required"`
+	TurnTimeStatus string `json:"turn_time_status" binding:"required"`
+	BookingDate    string `json:"booking_date" binding:"required"`
+	Tee            string `json:"tee" binding:"required"`
+	Note           string `json:"note"`
 }
 type GetListLockTurn struct {
 	PageRequest
-	PartnerUid string `form:"partner_uid"`
-	CourseUid  string `form:"course_uid"`
-	TeeTime    string `form:"tee_time"`
+	PartnerUid     string `form:"partner_uid"`
+	CourseUid      string `form:"course_uid"`
+	BookingDate    string `json:"booking_date"`
+	TurnTimeStatus string `form:"turn_time_status"`
 }
