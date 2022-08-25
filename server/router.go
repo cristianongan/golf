663096b769faf3bd25c879934815523648bf1cce
Cao Van Hai <caohaibk92@gmail.com>
2022-08-25 13:42:45 +0700

    udp

--- server/router.go
+++ server/router.go
@@ -225,10 +225,10 @@ func NewRouter() *gin.Engine {
 
 			/// =================== Booking Service Item ===================
 			cBookingServiceItem := new(controllers.CBookingServiceItem)
-			cmsApiAuthorized.GET("/booking/booking-service-item/list", middlewares.AuthorizedCmsUserHandler(cBookingServiceItem.GetBookingServiceItemList))
-			cmsApiAuthorized.POST("/booking/booking-service-item/add", middlewares.AuthorizedCmsUserHandler(cBookingServiceItem.AddBookingServiceItemToBag))
-			cmsApiAuthorized.PUT("/booking/booking-service-item/:id", middlewares.AuthorizedCmsUserHandler(cBookingServiceItem.UdpBookingServiceItemToBag))
-			cmsApiAuthorized.DELETE("/booking/booking-service-item/:id", middlewares.AuthorizedCmsUserHandler(cBookingServiceItem.DelBookingServiceItemToBag))
+			cmsApiAuthorized.GET("/booking-service-item/list", middlewares.AuthorizedCmsUserHandler(cBookingServiceItem.GetBookingServiceItemList))
+			cmsApiAuthorized.POST("/booking-service-item/add", middlewares.AuthorizedCmsUserHandler(cBookingServiceItem.AddBookingServiceItemToBag))
+			cmsApiAuthorized.PUT("/booking-service-item/:id", middlewares.AuthorizedCmsUserHandler(cBookingServiceItem.UdpBookingServiceItemToBag))
+			cmsApiAuthorized.DELETE("/booking-service-item/:id", middlewares.AuthorizedCmsUserHandler(cBookingServiceItem.DelBookingServiceItemToBag))
 
 			/// =================== Checkout ===================
 			cmsApiAuthorized.POST("/booking/checkout", middlewares.AuthorizedCmsUserHandler(cBooking.Checkout))
