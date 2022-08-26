1a52db42dd59605544d4c1e4c43c90724cd77a79
Nguyen Quoc Anh <anhnq@caro.vn>
2022-08-26 14:19:34 +0700

    upd

--- controllers/CCustomerUser.go
+++ controllers/CCustomerUser.go
@@ -161,24 +161,26 @@ func (_ *CCustomerUser) UpdateCustomerUser(c *gin.Context, prof models.CmsUser) 
 	}
 
 	if body.Phone != "" {
-		cusTemp := models.CustomerUser{
-			PartnerUid: body.PartnerUid,
-			CourseUid:  body.CourseUid,
-			Phone:      body.Phone,
-		}
-
-		errFind := cusTemp.FindFirst()
-		if errFind == nil || cusTemp.Uid != "" {
-			// đã tồn tại
-			res := map[string]interface{}{
-				"message":     "Số điện thoại đã tồn tại",
-				"status_code": 400,
-				"user":        cusTemp,
+		if customerUser.Phone != body.Phone {
+			cusTemp := models.CustomerUser{
+				PartnerUid: body.PartnerUid,
+				CourseUid:  body.CourseUid,
+				Phone:      body.Phone,
 			}
-			c.JSON(400, res)
-			return
-		} else {
-			customerUser.Phone = body.Phone
+
+			errFind := cusTemp.FindFirst()
+			if errFind == nil || cusTemp.Uid != "" {
+				// đã tồn tại
+				res := map[string]interface{}{
+					"message":     "Số điện thoại đã tồn tại",
+					"status_code": 400,
+					"user":        cusTemp,
+				}
+				c.JSON(400, res)
+				return
+			} else {
+				customerUser.Phone = body.Phone
+			}
 		}
 	}
 
@@ -188,24 +190,26 @@ func (_ *CCustomerUser) UpdateCustomerUser(c *gin.Context, prof models.CmsUser) 
 		customerUser.Status = body.Status
 	}
 	if body.Identify != "" {
-		cusTemp := models.CustomerUser{
-			PartnerUid: body.PartnerUid,
-			CourseUid:  body.CourseUid,
-			Identify:   body.Identify,
-		}
-
-		errFind := cusTemp.FindFirst()
-		if errFind == nil || cusTemp.Uid != "" {
-			// đã tồn tại
-			res := map[string]interface{}{
-				"message":     "Số chứng minh thư đã tồn tại",
-				"status_code": 400,
-				"user":        cusTemp,
+		if customerUser.Identify != body.Identify {
+			cusTemp := models.CustomerUser{
+				PartnerUid: body.PartnerUid,
+				CourseUid:  body.CourseUid,
+				Identify:   body.Identify,
 			}
-			c.JSON(400, res)
-			return
-		} else {
-			customerUser.Identify = body.Identify
+
+			errFind := cusTemp.FindFirst()
+			if errFind == nil || cusTemp.Uid != "" {
+				// đã tồn tại
+				res := map[string]interface{}{
+					"message":     "Số chứng minh thư đã tồn tại",
+					"status_code": 400,
+					"user":        cusTemp,
+				}
+				c.JSON(400, res)
+				return
+			} else {
+				customerUser.Identify = body.Identify
+			}
 		}
 	}
 	if body.Type != "" {
