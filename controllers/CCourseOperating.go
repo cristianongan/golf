a03ecf15981e139d83a9d4d7f5bdac639930c990
tuanduy183 <70496851+tuanduy183@users.noreply.github.com>
2022-09-05 11:52:20 +0700

    pending create flight

--- controllers/CCourseOperating.go
+++ controllers/CCourseOperating.go
@@ -1,7 +1,6 @@
 package controllers
 
 import (
-	"errors"
 	"log"
 	"start/constants"
 	"start/controllers/request"
@@ -128,14 +127,14 @@ func (_ *CCourseOperating) CreateFlight(c *gin.Context, prof models.CmsUser) {
 	listCaddieInOut := []model_gostarter.CaddieInOutNote{}
 	for _, v := range body.ListData {
 		errB, bookingTemp, caddieTemp, buggyTemp := addCaddieBuggyToBooking(body.PartnerUid, body.CourseUid, body.BookingDate, v.Bag, v.CaddieCode, v.BuggyCode)
-		isCaddiReady := true
+		// isCaddiReady := true
 
-		if !(caddieTemp.CurrentStatus == constants.CADDIE_CURRENT_STATUS_READY || caddieTemp.CurrentStatus == constants.CADDIE_CURRENT_STATUS_FINISH || caddieTemp.CurrentStatus == constants.CADDIE_CURRENT_STATUS_LOCK) {
-			listError = append(listError, errors.New(caddieTemp.Code+" chưa sẵn sàng để ghép ").Error())
-			isCaddiReady = false
-		}
+		// if !(caddieTemp.CurrentStatus == constants.CADDIE_CURRENT_STATUS_READY || caddieTemp.CurrentStatus == constants.CADDIE_CURRENT_STATUS_FINISH || caddieTemp.CurrentStatus == constants.CADDIE_CURRENT_STATUS_LOCK) {
+		// 	listError = append(listError, errors.New(caddieTemp.Code+" chưa sẵn sàng để ghép ").Error())
+		// 	isCaddiReady = false
+		// }
 
-		if errB == nil && isCaddiReady {
+		if errB == nil {
 			// Update caddie_current_status
 			caddieTemp.CurrentStatus = constants.CADDIE_CURRENT_STATUS_IN_COURSE
 			caddieTemp.CurrentRound = caddieTemp.CurrentRound + 1
