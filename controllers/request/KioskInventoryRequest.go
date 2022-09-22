a3219609539620de23fc65b82be44b4716e7d7c4
tuanduy183 <70496851+tuanduy183@users.noreply.github.com>
2022-09-22 09:39:11 +0700

    update filter name, type service inventory

--- controllers/request/KioskInventoryRequest.go
+++ controllers/request/KioskInventoryRequest.go
@@ -46,13 +46,14 @@ type CreateOutputBillBody struct {
 
 type GetInOutItems struct {
 	PageRequest
-	ServiceId  int64  `form:"service_id" binding:"required"`
-	PartnerUid string `form:"partner_uid" binding:"required"`
-	CourseUid  string `form:"course_uid" binding:"required"`
-	ItemCode   string `form:"item_code"`
-	FromDate   string `form:"from_date"`
-	ToDate     string `form:"to_date"`
-	Type       string `form:"type"`
+	ServiceId   int64  `form:"service_id" binding:"required"`
+	PartnerUid  string `form:"partner_uid" binding:"required"`
+	CourseUid   string `form:"course_uid" binding:"required"`
+	ItemCode    string `form:"item_code"`
+	FromDate    string `form:"from_date"`
+	ToDate      string `form:"to_date"`
+	Type        string `form:"type"`
+	ProductName string `form:"product_name"`
 }
 
 type GetItems struct {
