0b6693422b135c7dc3a7dc052350ce7e30737d5d
Nguyen Quoc Anh <anhnq@caro.vn>
2022-10-04 17:41:32 +0700

    update

---
+++ controllers/request/RServiceOTA.go
@@ -0,0 +1,15 @@
+package request
+
+type ServiceGolfDataBody struct {
+	Token      string `json:"Token"`
+	CourseCode string `json:"CourseCode"`
+}
+
+type CheckServiceGolfBody struct {
+	Token      string `json:"Token"`
+	CourseCode string `json:"CourseCode"`
+	RenTalCode string `json:"RenTalCode"`
+	CaddieNo   string `json:"CaddieNo"`
+	DateStr    string `json:"DateStr"`
+	TeeOffStr  string `json:"TeeOffStr"`
+}
