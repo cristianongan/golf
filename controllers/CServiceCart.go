d5f00616139c1cbbfc2256b19d115d4845a5b383
Nguyen Quoc Anh <anhnq@caro.vn>
2022-10-20 10:49:18 +0700

    update api kiosk

--- controllers/CServiceCart.go
+++ controllers/CServiceCart.go
@@ -179,10 +179,10 @@ func (_ CServiceCart) AddItemServiceToCart(c *gin.Context, prof models.CmsUser) 
 		}
 	} else {
 		// Kiểm tra trạng thái bill
-		if serviceCart.BillStatus != constants.POS_BILL_STATUS_PENDING {
-			response_message.BadRequest(c, "Bill status invalid")
-			return
-		}
+		// if serviceCart.BillStatus != constants.POS_BILL_STATUS_PENDING {
+		// 	response_message.BadRequest(c, "Bill status invalid")
+		// 	return
+		// }
 		// update tổng giá bill
 		serviceCart.Amount += body.Quantity * serviceCartItem.UnitPrice
 		if err := serviceCart.Update(db); err != nil {
@@ -478,10 +478,10 @@ func (_ CServiceCart) UpdateItemCart(c *gin.Context, prof models.CmsUser) {
 	}
 
 	// Kiểm tra trạng thái bill
-	if serviceCart.BillStatus != constants.POS_BILL_STATUS_PENDING {
-		response_message.BadRequest(c, "Bill status invalid")
-		return
-	}
+	// if serviceCart.BillStatus != constants.POS_BILL_STATUS_PENDING {
+	// 	response_message.BadRequest(c, "Bill status invalid")
+	// 	return
+	// }
 
 	if body.Quantity > 0 {
 		// validate quantity
