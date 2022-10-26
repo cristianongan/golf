456d543370b8ccdcc892a9fd3dc6719729b37a3a
tuanduy183 <70496851+tuanduy183@users.noreply.github.com>
2022-10-26 14:32:27 +0700

    update inventory

--- cron/cron_inventory_statistic.go
+++ cron/cron_inventory_statistic.go
@@ -16,9 +16,7 @@ func runReportInventoryStatisticItemJob() {
 	runReportInventoryStatisticItem()
 }
 
-// Báo cáo số fee của caddie trong ngày,
 func runReportInventoryStatisticItem() {
-	//Lấy danh sách caddie in out note trong ngày
 	db := datasources.GetDatabase()
 	cStatistic := controllers.CStatisticItem{}
 	cStatistic.AddItemToStatistic(db)
--- server/router.go
+++ server/router.go
@@ -558,7 +558,7 @@ func NewRouter() *gin.Engine {
 			/// =================== Kiosk Statistic ===================
 			cKioskStatistic := new(controllers.CStatisticItem)
 			cmsApiAuthorized.GET("/kiosk-inventory/item/statistic/list", middlewares.AuthorizedCmsUserHandler(cKioskStatistic.GetItemStatisticDetail))
-			// cmsApiAuthorized.POST("/kiosk-inventory/item/statistic/create", middlewares.AuthorizedCmsUserHandler(cKioskStatistic.TestAddItemToStatistic))
+			cmsApiAuthorized.POST("/kiosk-inventory/item/statistic/create", middlewares.AuthorizedCmsUserHandler(cKioskStatistic.TestAddItemToStatistic))
 
 			/// =================== Kiosk ===================
 			cServiceCart := new(controllers.CServiceCart)
