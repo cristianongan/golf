16c028b4affaa40ddeda3d66fa87c094738ddb3f
tuanduy183 <70496851+tuanduy183@users.noreply.github.com>
2022-11-03 15:15:22 +0700

    update api get list booking : fix filter check_in_time

--- controllers/CBooking.go
+++ controllers/CBooking.go
@@ -717,7 +717,7 @@ func (_ *CBooking) GetListBookingWithSelect(c *gin.Context, prof models.CmsUser)
 	}
 
 	var list []model_booking.Booking
-	db.Find(&list)
+	db.Debug().Find(&list)
 	res = response.PageResponse{
 		Total: total,
 		Data:  list,
