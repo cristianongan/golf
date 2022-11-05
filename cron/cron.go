e2fe812c34aae1ba9ecc04b01208dc9757146c61
Cao Van Hai <caohaibk92@gmail.com>
2022-11-05 11:24:38 +0700

    udp cron caddie fee

--- cron/cron.go
+++ cron/cron.go
@@ -10,7 +10,7 @@ import (
 func CronStart() {
 	c := cron.New()
 
-	c.AddFunc("CRON_TZ=Asia/Ho_Chi_Minh 10 11 * * *", runReportCaddieFeeToDay)            // Chạy lúc 23h00 hàng ngày
+	c.AddFunc("CRON_TZ=Asia/Ho_Chi_Minh 05 23 * * *", runReportCaddieFeeToDay)            // Chạy lúc 23h00 hàng ngày
 	c.AddFunc("CRON_TZ=Asia/Ho_Chi_Minh 10 00 * * *", runResetDataMemberCardJob)          // Chạy lúc 00h10 sáng hàng ngày để reset data trong ngày của member card
 	c.AddFunc("CRON_TZ=Asia/Ho_Chi_Minh 00 00 * * *", runReportInventoryStatisticItemJob) // Chạy lúc 0h sáng hàng ngày để thống kê sản phẩm trong kiosk inventory
 	c.AddFunc("CRON_TZ=Asia/Ho_Chi_Minh 30 23 * * *", runBookingLogutJob)                 // Chạy lúc 23h30 tối hàng ngày để logout các booking chưa checkout
