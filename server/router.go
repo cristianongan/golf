5b6bf1e3843131a546c279e525787feced94ab30
tuanduy183 <70496851+tuanduy183@users.noreply.github.com>
2022-11-09 09:46:36 +0700

    update api maintain schedule

--- server/router.go
+++ server/router.go
@@ -637,8 +637,8 @@ func NewRouter() *gin.Engine {
 			cmsApiAuthorized.POST("/valet/add-bag-caddie-buggy", middlewares.AuthorizedCmsUserHandler(cValet.AddBagCaddieBuggyToBooking))
 			/// =================== Caddie Working Schedule ===================
 			cMaintainSchedule := new(controllers.CMaintainSchedule)
-			cmsApiAuthorized.GET("/maintain-schedule", middlewares.AuthorizedCmsUserHandler(cMaintainSchedule.GetMaintainScheduleList))
-			cmsApiAuthorized.POST("/maintain-schedule/list", middlewares.AuthorizedCmsUserHandler(cMaintainSchedule.CreateMaintainSchedule))
+			cmsApiAuthorized.POST("/maintain-schedule", middlewares.AuthorizedCmsUserHandler(cMaintainSchedule.CreateMaintainSchedule))
+			cmsApiAuthorized.GET("/maintain-schedule/list", middlewares.AuthorizedCmsUserHandler(cMaintainSchedule.GetMaintainScheduleList))
 			cmsApiAuthorized.PUT("/maintain-schedule/:id", middlewares.AuthorizedCmsUserHandler(cMaintainSchedule.UpdateMaintainSchedule))
 
 			/// =================== Notification ===================
@@ -657,6 +657,7 @@ func NewRouter() *gin.Engine {
 			cmsApiAuthorized.POST("/buggy-fee-item-setting", middlewares.AuthorizedCmsUserHandler(cBuggyFeeItemSetting.CreateBuggyFeeItemSetting))
 			cmsApiAuthorized.GET("/buggy-fee-item-setting/list", middlewares.AuthorizedCmsUserHandler(cBuggyFeeItemSetting.GetBuggyFeeItemSettingList))
 			cmsApiAuthorized.DELETE("/buggy-fee-item-setting/:id", middlewares.AuthorizedCmsUserHandler(cBuggyFeeItemSetting.DeleteBuggyFeeSetting))
+			// cmsApiAuthorized.PUT("/buggy-fee-item-setting/:id", middlewares.AuthorizedCmsUserHandler(cBuggyFeeItemSetting.UpdateBuggyFeeItemSetting))
 		}
 
 		// ----------------------------------------------------------
