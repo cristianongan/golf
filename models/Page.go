08b48f0e6a58a6e8e99e7e34ccb6709b9ab1c60e
quyetvv <quyetvv@caro.vn>
2022-11-15 17:16:39 +0700

    fix sql injection sort_by param

--- models/Page.go
+++ models/Page.go
@@ -1,6 +1,11 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"regexp"
+	"strings"
+
+	"gorm.io/gorm"
+)
 
 type Page struct {
 	Limit   int
@@ -17,6 +22,12 @@ func (p *Page) Setup(db *gorm.DB) *gorm.DB {
 	var setupDb = db
 	setupDb = setupDb.Offset(p.Offset()).Limit(p.Limit)
 	if p.SortBy != "" && p.SortDir != "" {
+		if strings.ToUpper(p.SortDir) != "ASC" && strings.ToUpper(p.SortDir) != "DESC" {
+			p.SortBy = "desc"
+		}
+		pattern := "[^a-zA-Z0-9_]"
+		output := regexp.MustCompile(pattern).ReplaceAllString(p.SortBy, "")
+		p.SortBy = output
 		setupDb = setupDb.Order(p.SortBy + " " + p.SortDir)
 	}
 	return setupDb
