4c3afa6982229449f00ed92860c472ec89178359
Nguyen Quoc Anh <anhnq@caro.vn>
2022-12-16 17:47:06 +0700

    upd

--- controllers/response/BuggyListResponse.go
+++ controllers/response/BuggyListResponse.go
@@ -8,18 +8,19 @@ import (
 // BuggyListResponse : Booking
 type BuggyListResponse struct {
 	models.Model
-	PartnerUid   string                       `json:"partner_uid,omitempty"`
-	CourseUid    string                       `json:"course_uid,omitempty"`
-	BookingDate  string                       `json:"booking_date,omitempty"`
-	BuggyId      string                       `json:"buggy_id,omitempty"`
-	BuggyInfo    *model_booking.BookingBuggy  `json:"buggy_info,omitempty"`
-	CaddieId     string                       `json:"caddie_id,omitempty"`
-	CaddieInfo   *model_booking.BookingCaddie `json:"caddie_info,omitempty"`
-	Bag          string                       `json:"bag,omitempty"`
-	CustomerName string                       `json:"customer_name,omitempty"`
-	CustomerInfo *model_booking.CustomerInfo  `json:"customer_info,omitempty"`
-	Hole         int                          `json:"hole,omitempty"`
-	FlightId     int64                        `json:"flight_id,omitempty"`
-	TeeType      string                       `json:"tee_type"`
-	TeeOffTime   string                       `json:"tee_off_time"`
+	PartnerUid     string                       `json:"partner_uid,omitempty"`
+	CourseUid      string                       `json:"course_uid,omitempty"`
+	BookingDate    string                       `json:"booking_date,omitempty"`
+	BuggyId        string                       `json:"buggy_id,omitempty"`
+	BuggyInfo      *model_booking.BookingBuggy  `json:"buggy_info,omitempty"`
+	CaddieId       string                       `json:"caddie_id,omitempty"`
+	CaddieInfo     *model_booking.BookingCaddie `json:"caddie_info,omitempty"`
+	Bag            string                       `json:"bag,omitempty"`
+	CustomerName   string                       `json:"customer_name,omitempty"`
+	CustomerInfo   *model_booking.CustomerInfo  `json:"customer_info,omitempty"`
+	Hole           int                          `json:"hole,omitempty"`
+	FlightId       int64                        `json:"flight_id,omitempty"`
+	TeeType        string                       `json:"tee_type"`
+	TeeOffTime     string                       `json:"tee_off_time"`
+	IsPrivateBuggy *bool                        `json:"is_private_buggy"`
 }
