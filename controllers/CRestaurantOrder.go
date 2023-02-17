ee416d26ba14cab529da752f5472c9f104a7d559
Quoc Anh <anhnq@caro.vn>
2023-02-18 00:00:30 +0700

    upd

--- controllers/CRestaurantOrder.go
+++ controllers/CRestaurantOrder.go
@@ -249,6 +249,65 @@ func (_ CRestaurantOrder) DeleteRestaurantOrder(c *gin.Context, prof models.CmsU
 	okRes(c)
 }
 
+// Xoa don
+func (_ CRestaurantOrder) DeleteOrder(c *gin.Context, prof models.CmsUser) {
+	db := datasources.GetDatabaseWithPartner(prof.PartnerUid)
+	idRequest := c.Param("id")
+	id, errId := strconv.ParseInt(idRequest, 10, 64)
+	if errId != nil {
+		response_message.BadRequest(c, errId.Error())
+		return
+	}
+
+	// validate cart
+	serviceCart := models.ServiceCart{}
+	serviceCart.Id = id
+	serviceCart.PartnerUid = prof.PartnerUid
+	serviceCart.CourseUid = prof.CourseUid
+
+	if err := serviceCart.FindFirst(db); err != nil {
+		response_message.BadRequest(c, err.Error())
+		return
+	}
+
+	// validate golf bag
+	booking := model_booking.Booking{}
+	booking.Uid = serviceCart.BookingUid
+
+	if err := booking.FindFirst(db); err != nil {
+		response_message.BadRequest(c, "Booking "+err.Error())
+		return
+	}
+
+	if err := serviceCart.Delete(db); err != nil {
+		response_message.InternalServerError(c, err.Error())
+		return
+	}
+
+	//find all item in bill
+	restaurantItem := models.RestaurantItem{}
+	restaurantItem.BillId = serviceCart.Id
+
+	list, err := restaurantItem.FindAll(db)
+
+	if err != nil {
+		response_message.BadRequest(c, err.Error())
+		return
+	}
+
+	for _, item := range list {
+		if err := item.Delete(db); err != nil {
+			response_message.BadRequest(c, err.Error())
+			return
+		}
+	}
+
+	//Update lại giá trong booking
+	updatePriceWithServiceItem(booking, prof)
+
+	okRes(c)
+}
+
 func (_ CRestaurantOrder) GetListBill(c *gin.Context, prof models.CmsUser) {
 	db := datasources.GetDatabaseWithPartner(prof.PartnerUid)
 	query := request.GetListBillBody{}
