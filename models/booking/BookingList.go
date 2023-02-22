c71a0454077ba2abbab20b6c94ce09d2e91082b4
tuanduy183 <70496851+tuanduy183@users.noreply.github.com>
2023-02-22 18:24:36 +0700

    update filter report starter

--- models/booking/BookingList.go
+++ models/booking/BookingList.go
@@ -544,7 +544,9 @@ func (item *BookingList) FindReportStarter(database *gorm.DB, page models.Page) 
 	db := database.Model(Booking{})
 
 	db = addFilter(db, item, false)
-	db.Group("bag")
+	db = db.Where("customer_type <> ?", constants.CUSTOMER_TYPE_NONE_GOLF)
+	db = db.Where("check_in_time > 0")
+	db = db.Group("bag")
 	db.Select("*, SUM(hole_time_out) as hole_played")
 
 	db.Count(&total)
