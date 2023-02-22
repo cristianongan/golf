8926f19b49130a8943b05753a6269a3d040d8ccd
Nguyen Quoc Anh <anhnq@caro.vn>
2023-02-22 17:43:33 +0700

    upd

--- models/booking/BookingList.go
+++ models/booking/BookingList.go
@@ -439,13 +439,13 @@ func (item *BookingList) FindListBookingWithBuggy(database *gorm.DB, page models
 
 	db = db.Where("bookings.buggy_info->'$.code' <> ''")
 	db = db.Where("booking_service_items.service_type = ?", constants.BUGGY_SETTING)
-	db = db.Joins("JOIN booking_service_items ON booking_service_items.bill_code = bookings.bill_code")
+	db = db.Joins("JOIN booking_service_items ON booking_service_items.booking_uid = bookings.uid")
 	db = db.Select("bookings.booking_date, JSON_VALUE(bookings.buggy_info,'$.code') as buggy_code, booking_service_items.name as buggy_type, bookings.bag, bookings.tee_off_time as tee_off, bookings.guest_style_name, bookings.guest_style, bookings.card_id, JSON_VALUE(bookings.agency_info,'$.name') as agency_name, bookings.hole, bookings.caddie_id, booking_service_items.amount as fee")
 
 	db.Count(&total)
 
 	if total > 0 && int64(page.Offset()) < total {
-		db = page.Setup(db).Find(&list)
+		db = page.Setup(db).Debug().Find(&list)
 	}
 
 	return list, total, db.Error
