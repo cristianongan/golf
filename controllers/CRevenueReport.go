38339fed54babb113569aa2532cfa2bc3e2489df
tuanduy183 <70496851+tuanduy183@users.noreply.github.com>
2023-03-06 10:57:23 +0700

    update BC pos

--- controllers/CRevenueReport.go
+++ controllers/CRevenueReport.go
@@ -127,11 +127,11 @@ func (cRevenueReport *CRevenueReport) GetBookingReportRevenueDetail(c *gin.Conte
 	}
 
 	reportRevenue := model_report.ReportRevenueDetailList{
-		PartnerUid:  form.PartnerUid,
-		CourseUid:   form.CourseUid,
-		GuestStyle:  form.GuestStyle,
-		BookingDate: form.BookingDate,
-		Bag:         form.Bag,
+		PartnerUid: form.PartnerUid,
+		CourseUid:  form.CourseUid,
+		GuestStyle: form.GuestStyle,
+		FromDate:   form.FromDate,
+		ToDate:     form.ToDate,
 	}
 
 	list, total, _ := reportRevenue.FindBookingRevenueList(db, page)
