69f291adaabe594b67204c5248f027fec55fa121
tuanduy183 <70496851+tuanduy183@users.noreply.github.com>
2023-03-09 10:14:14 +0700

    update socket room

--- server/router.go
+++ server/router.go
@@ -6,7 +6,9 @@ import (
 	"start/config"
 	"start/controllers"
 	"start/middlewares"
-	"start/socket"
+
+	// "start/socket"
+	socket "start/socket_room"
 	"strings"
 
 	"github.com/gin-gonic/gin"
@@ -44,13 +46,13 @@ func NewRouter() *gin.Engine {
 	router.Group(moduleName).GET("/", healthcheck)
 	router.Group(moduleName).GET("/healthz", healthcheck)
 	router.Group(moduleName).GET("/ws", func(c *gin.Context) {
-		socket.ServeWs(c.Writer, c.Request)
+		// socket.ServeWs(c.Writer, c.Request)
 	})
 
-	// router.Group(moduleName).GET("/ws/:roomId", func(c *gin.Context) {
-	// 	roomId := c.Param("roomId")
-	// 	socket_room.ServeWs(c.Writer, c.Request, roomId)
-	// })
+	router.Group(moduleName).GET("/ws/:roomId", func(c *gin.Context) {
+		roomId := c.Param("roomId")
+		socket.ServeWs(c.Writer, c.Request, roomId)
+	})
 
 	if config.GetKibanaLog() {
 		router.Use(middlewares.GinBodyLogMiddleware)
