212f386691861bf387707660bd04aba30225ab7b
Cao Van Hai <caohaibk92@gmail.com>
2023-03-14 12:04:36 +0700

    udp operation log

--- controllers/CGolfLog.go
+++ controllers/CGolfLog.go
@@ -2,8 +2,11 @@ package controllers
 
 import (
 	"log"
+	"start/constants"
 	"start/controllers/request"
+	"start/datasources"
 	"start/models"
+	model_booking "start/models/booking"
 	"start/utils/response_message"
 
 	"github.com/gin-gonic/gin"
@@ -15,6 +18,11 @@ func (_ *CGolfLog) GetGolfLogList(c *gin.Context, prof models.CmsUser) {
 	form := request.GetOperationLogForm{}
 	if bindErr := c.ShouldBind(&form); bindErr != nil {
 		response_message.BadRequest(c, bindErr.Error())
+		return
+	}
+
+	if form.BookingDate == "" || len(form.BookingDate) < 8 {
+		response_message.BadRequest(c, "GetGolfLogList err booking date invalid")
 		return
 	}
 
@@ -41,6 +49,29 @@ func (_ *CGolfLog) GetGolfLogList(c *gin.Context, prof models.CmsUser) {
 	if err != nil {
 		response_message.InternalServerError(c, err.Error())
 		return
+	}
+
+	if form.Bag != "" {
+		// Get thêm lúc tạo booking
+		booking := model_booking.Booking{
+			Bag:         form.Bag,
+			BookingDate: form.BookingDate,
+		}
+		db := datasources.GetDatabaseWithPartner(form.PartnerUid)
+		errFF := booking.FindFirst(db)
+		if errFF == nil {
+			opLogBag := models.OperationLog{
+				BookingUid: booking.Uid,
+				Action:     constants.OP_LOG_ACTION_CREATE,
+			}
+			errFS := opLogBag.FindFirst()
+			if errFS == nil {
+				listTemp := []models.OperationLog{}
+				listTemp = append(listTemp, opLogBag)
+				list = append(listTemp, list...)
+				total += 1
+			}
+		}
 	}
 
 	res := map[string]interface{}{
