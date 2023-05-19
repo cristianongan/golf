8a621c79960621fb4d1b41a6124340bb557ecabd
DESKTOP-PI3M949\CARO-PC <caohaibk92@gmail.com>
2023-05-19 16:03:26 +0700

    add log file size

--- controllers/CMemberCard.go
+++ controllers/CMemberCard.go
@@ -68,6 +68,7 @@ func (_ *CMemberCard) EKycUpdateImageMemberCard(c *gin.Context, prof models.CmsU
 		return
 	}
 	fileSize := file.(Sizer).Size()
+	log.Println("EKycUpdateImageMemberCard fileSize", fileSize)
 	if fileSize > constants.MAX_SIZE_AVATAR_UPLOAD_EKYC {
 		response_message.BadRequest(c, "over limit size")
 		return
