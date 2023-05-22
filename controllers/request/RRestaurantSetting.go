7a0ad40962e078270a688822adb0a1b9c45a728f
Nguyen Quoc Anh <anhnq@caro.vn>
2023-05-22 10:06:50 +0700

    upd

--- controllers/request/RRestaurantSetting.go
+++ controllers/request/RRestaurantSetting.go
@@ -1,17 +1,17 @@
 package request
 
 type CreateRestaurantSettingBody struct {
-	PartnerUid  string   `json:"partner_uid" binding:"required"` // Hãng Golf
-	CourseUid   string   `json:"course_uid" binding:"required"`  // Sân Golf
-	BookingDate string   `json:"booking_date"`                   //  Sân
-	Course      string   `json:"course"`                         //  Sân
-	Hole        int      `json:"hole"`                           // Số hố
-	HoleIndex   int      `json:"hole_index"`                     // Số thứ tự của hố
-	Par         int      `json:"par"`                            // Số lần chạm gậy
-	TimeStart   int64    `json:"time_start"`
-	TimeEnd     int64    `json:"time_end"`
-	FlightId    int64    `json:"flight_id" binding:"required"`
-	Players     []Player `json:"players"`
+	PartnerUid    string   `json:"partner_uid" binding:"required"` // Hãng Golf
+	CourseUid     string   `json:"course_uid" binding:"required"`  // Sân Golf
+	ServiceIds    []int    `json:"service_ids" binding:"required"`
+	Name          string   `json:"name" binding:"required"`          // Tên setting
+	NumberTables  int      `json:"number_tables" binding:"required"` // Số bàn
+	PeopleInTable int      `json:"people_in_table"`                  //  Tổng số người trong 1 bàn
+	Type          string   `json:"type"`                             // Loại setting
+	Time          int      `json:"time"`                             // Số phút setting
+	Symbol        string   `json:"symbol"`                           // Ký hiệu
+	TableFrom     int      `json:"table_from"`                       //
+	DataTables    []string `json:"data_tables"`                      //
 }
 
 type GetListRestaurantSettingForm struct {
