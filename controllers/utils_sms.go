d47465398507d49d8ad6bdb48d91d4975fc1e450
Nganph <nganph@caro.vn>
2023-05-26 15:30:45 +0700

    gioi han do dai tin nhan booking

--- controllers/utils_sms.go
+++ controllers/utils_sms.go
@@ -109,19 +109,12 @@ func genQRCodeListBook(listBooking []model_booking.Booking) {
 /*
 Giới hạn độ dài tin nhắn
 */
-func sendSmsBooking(listBooking []model_booking.Booking, phone string) error {
-	if len(listBooking) == 0 {
-		errEmpty := errors.New("sendSmsBooking Err List Booking Emplty")
-		log.Println("sendSmsBooking errEmpty", errEmpty.Error())
-		return errEmpty
-	}
-
+func sendSmsBooking(listBooking []model_booking.Booking, phone string) {
 	part := 10 // tối đa 10 booking 1 tin nhắn
 	count := 0
-	var err error
 
 	if len(listBooking) <= part {
-		err = makeSendSmsBooking(listBooking, phone)
+		makeSendSmsBooking(listBooking, phone)
 	} else {
 		for {
 			end := count + part
@@ -129,21 +122,25 @@ func sendSmsBooking(listBooking []model_booking.Booking, phone string) error {
 				end = len(listBooking)
 			}
 
-			err = makeSendSmsBooking(listBooking[count:end], phone)
+			makeSendSmsBooking(listBooking[count:end], phone)
 			count = end
-			if count >= len(listBooking) || err != nil {
+			if count >= len(listBooking) {
 				break
 			}
 		}
 	}
-
-	return err
 }
 
 /*
 Send sms
 */
 func makeSendSmsBooking(listBooking []model_booking.Booking, phone string) error {
+	if len(listBooking) == 0 {
+		errEmpty := errors.New("sendSmsBooking Err List Booking Emplty")
+		log.Println("sendSmsBooking errEmpty", errEmpty.Error())
+		return errEmpty
+	}
+
 	// parse standard phone number
 	num, errPhone := libphonenumber.Parse(phone, "VN")
 	if errPhone != nil {
